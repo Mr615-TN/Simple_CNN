7d0654dcacb259db5713dad0428cce727c3b48e9
Mr615-TN <tneema501@gmail.com>
2024-09-20 23:07:16 -0500

    working on first cnn

--- firstCNN.py
+++ firstCNN.py
@@ -23,4 +23,25 @@ class SimpleCNN(nn.Module):
 
 device = torch.device("cpu")
 
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
+trainset = torchvision.datasets.CIFAR10(root='./data', download = True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=32, shuffle=True, num_workers=2)
 
+model = SimpleCNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 5 
+for epoch in range(num_epochs):
+    for i, data in enumerate(trainloader,0):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 200 = 199;
+    print(f'[{epoch + 1}. {i+1}] loss: {running_loss / 200:.3f}')
+
+print('Finished the Training')
