494ecf4e4c840634dcb1d67150efef7e50b558b6
Mr615-TN <tneema501@gmail.com>
2024-09-22 18:16:11 -0500

    working on firstCNN.py

--- firstCNN.py
+++ firstCNN.py
@@ -24,7 +24,7 @@ class SimpleCNN(nn.Module):
 device = torch.device("cpu")
 
 transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
-trainset = torchvision.datasets.CIFAR10(root='./data', download = True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data',train=True, download = True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset,batch_size=32, shuffle=True, num_workers=2)
 
 model = SimpleCNN().to(device)
@@ -44,4 +44,25 @@ for epoch in range(num_epochs):
         if i % 200 = 199;
     print(f'[{epoch + 1}. {i+1}] loss: {running_loss / 200:.3f}')
 
+
 print('Finished the Training')
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size,shuffle=False,num_workers=2)
+
+model.eval()
+correct=0 
+total=0 
+
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs=model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy on the test set: {100 * correct/total:.2f}%')
+
+torch.save(model.state_dict(), 'simple_cnn.pth')
+print("model saved to simple_cnn.pth")
