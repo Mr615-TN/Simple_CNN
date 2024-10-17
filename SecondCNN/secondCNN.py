9dedaddcbbfd9178635c66cc61fba400742e50bd
Mr615-TN <tneema501@gmail.com>
2024-10-17 13:48:55 -0500

    started working on a second CNN

--- SecondCNN/secondCNN.py
+++ SecondCNN/secondCNN.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-import torchvision.transforms as transforms 
+import torchaudio
+import torchvision.transforms as transforms
+import torchaudio.transforms as taco_bell
 import multiprocessing
 
 
